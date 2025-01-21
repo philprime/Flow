ba8def8d2f2315afb290b06737188f5c11439d35
Philip Niedertscheider <me@philprime.dev>
2025-01-21 15:08:50 +0100

    tests: replace Quick & Nimble with XCTest

--- Package.swift
+++ Package.swift
@@ -7,12 +7,8 @@ let package = Package(
     products: [
         .library(name: "Flow", targets: ["Flow"]),
     ],
-    dependencies: [
-        .package(url: "https://github.com/Quick/Quick", .upToNextMajor(from: "3.0.0")),
-        .package(url: "https://github.com/Quick/Nimble",  .upToNextMajor(from: "9.0.0"))
-    ],
     targets: [
         .target(name: "Flow"),
-        .testTarget(name: "FlowTests", dependencies: ["Flow", "Quick", "Nimble"]),
+        .testTarget(name: "FlowTests", dependencies: ["Flow"]),
     ]
 )
--- Tests/FlowTests/FlowTests.swift
+++ Tests/FlowTests/FlowTests.swift
@@ -1,36 +1,24 @@
-import Quick
-import Nimble
+import XCTest
 @testable import Flow
 
-final class FlowTests: QuickSpec {
-
-    override func spec() {
-        describe("not") {
-
-            context("predicate returns truthy") {
-
-                it("should negate a result") {
-                    expect(not({ true })(())) == false
-                }
-            }
-
-            context("predicate returns falsey") {
-
-                it("should negate a result") {
-                    expect(not({ false })(())) == true
-                }
-            }
-        }
-
-        describe("forceUnwrapMap") {
-
-            it("should return array with force unwrapped values") {
-                let values: [Int?] = [1, 2, 3, 4, 5]
-                let result = values.forceUnwrapMap({ value -> String? in
-                    String(describing: value)
-                })
-                expect(result) == ["Optional(1)", "Optional(2)", "Optional(3)", "Optional(4)", "Optional(5)"]
-            }
-        }
+final class FlowTests: XCTestCase {
+    
+    func testNotWithTruthyPredicate() {
+        XCTAssertFalse(not({ true })(()))
+    }
+    
+    func testNotWithFalseyPredicate() {
+        XCTAssertTrue(not({ false })(()))
+    }
+    
+    func testForceUnwrapMap() {
+        // -- Arrange --
+        let values: [Int?] = [1, 2, 3, 4, 5]
+        // -- Act --
+        let result = values.forceUnwrapMap({ value -> String? in
+            String(describing: value)
+        })
+        // -- Assert --
+        XCTAssertEqual(result, ["Optional(1)", "Optional(2)", "Optional(3)", "Optional(4)", "Optional(5)"])
     }
 }
